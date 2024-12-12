458683ab3198d477da996b38997679c2e3c0202a
Mohamed Sayed <ms7237322@gmail.com>
2024-12-12 20:21:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+// let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    // clearInterval(refreshInterval);
+    // refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+
+
+
+
+// Corner Video
+const videoContainer = document.querySelector('.video-container');
+const video = document.getElementById('video');
+const closeButton = document.querySelector('.close-button');
+const minusbutton = document.querySelector('.minus-button');
+
+videoContainer.addEventListener('click', () => {
+  if (videoContainer.classList.contains('active')) {
+    videoContainer.classList.remove('active');
+    video.muted = true;
+    video.pause();
+    video.style.width = '130px';
+    video.style.height = '180px';
+  } else {
+    videoContainer.classList.add('active');
+    video.muted = false;
+    video.play();
+  }
+});
+
+closeButton.addEventListener('click', (e) => {
+  e.stopPropagation();
+  videoContainer.classList.remove('active');
+  videoContainer.classList.add('closed');
+  video.muted = true;
+  video.pause();
+});
+minusbutton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    videoContainer.classList.remove('active');
+    video.muted = true;
+    video.pause();
+  });
+
+
+// Corner Plus
+
+function toggleIcons() {
+    const button = document.querySelector('.floating-button');
+    button.classList.toggle('open');
+  }
+  
